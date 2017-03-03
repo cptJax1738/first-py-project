7abc145318effe585de399390c2020ce81aa42e3
Yoh <cptjax@protonmail.ch>
2017-03-03 00:14:55 -0500

    first commit

---
+++ gitinit.sh
@@ -0,0 +1,7 @@
+echo "# first-py-project" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/cptJax1738/first-py-project.git
+git push -u origin master
+
